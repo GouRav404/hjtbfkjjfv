50b57a2ab10fbc5ad207f79d4e47e49868696407
Gaurav <gouravj44@gmail.com>
2023-05-29 03:02:34 +0530

    Update chat.js

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -10,17 +10,16 @@ export default async function handler(req, res) {
     if (!referer || referer !== process.env.APP_URL) {
       res.status(401).json({ message: 'Unauthorized' });
     }
-  }
-  else {
+  } else {
     try {
       const { body } = req;
       const url = 'https://api.openai.com/v1/chat/completions';
       const headers = {
         'Content-type': 'application/json',
-        { "Authorization": Bearer ${process.env.OPENAI_API_KEY} } }
+        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
       };
 
-      const response = await axios.post(url, body, { headers: headers })
+      const response = await axios.post(url, body, { headers: headers });
 
       res.status(200).json(response.data);
     } catch (error) {
@@ -28,5 +27,4 @@ export default async function handler(req, res) {
       res.status(500).json({ message: "Something went wrong" });
     }
   }
-  
 }
